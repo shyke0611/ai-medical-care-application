6fd06cfed193f9245ad2bb74be4dec2c773c7903
shyke0611 <hyungkenine2003@gmail.com>
2024-12-24 13:26:56 +1300

    set up basic func with test on linking up the backend with the frontend with api calls

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
-const App = () => {
-    return <h1>Hello, React!</h1>;
-};
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
